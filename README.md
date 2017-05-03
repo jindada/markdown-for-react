06781e1ddc0617d8ef32548f1b35b547a9fe774c
jzs <jinzhongshuai@antiy.cn>
2017-05-03 13:55:53 +0800

    update readme and example

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
+
 ## markdown-for-react
+
+[![NPM version](https://img.shields.io/npm/v/markdown-for-react.svg?style=flat)](https://npmjs.org/package/markdown-for-react)
+[![NPM downloads](http://img.shields.io/npm/dm/markdown-for-react.svg?style=flat)](https://npmjs.org/package/markdown-for-react)
+
+[live demo](https://jindada.github.io/markdown-for-react)
+
+> markdown for react component based on [markdown-it](https://github.com/markdown-it/markdown-it), [react-html-parser](https://github.com/wrakky/react-html-parser), [highlight.js](https://github.com/isagalaev/highlight.js)
+
+## Usage examples
+
+``` js
+import React from 'react';
+import { render } from 'react-dom';
+import MarkdownForReact from 'markdown-for-react';
+
+const text = `
+# h1 text
+## h2 text
+### h3 text
+#### h4 text
+##### h5 text
+###### \`h6 text\`
+\`\`\`js
+cnsole.log(hello markdown-for-react)
+\`\`\`
+`
+
+class Example extends React.Component {
+  render() {
+    return (
+      <div>
+      	<MarkdownForReact value={"# qweqwe"} />
+        <MarkdownForReact value={ text } />
+      </div>
+    )
+  }
+}
+render(<Example />, document.getElementById('init'));
+```
+
+
+## For Example
+
+# h1 text
+## h2 text
+### h3 text
+#### h4 text
+##### h5 text
+###### h6 text
+
+"double quotes" and 'single quotes'
+
+**bold text**
+
+__bold text__
+
+*italic text*
+
+_italic text_
+
+~~Strikethrough~~
+
+> china
+>> heilongjiang
+>>> harbin
+
++ China
+  - heilongjiang
+    * harin
+    + daqing
+    - suihua
++ England
+
+1. react
+2. vue
+3. angular
+1. jquery
+1. css
+
+57. heilongjiang
+1. beijing
+
+    // hello world
+    line 1
+    line 2
+    line 3
+
+## Tables
+
+| lib    |     url     |
+| ------ | ----------- |
+| react  | https://github.com/facebook/react |
+| vuejs  | https://github.com/vuejs/vue |
+| angular| https://github.com/angular/angular |
+
+
+| lib     |     url      |
+| ------: | -----------: |
+| react  | https://github.com/facebook/react |
+| vuejs  | https://github.com/vuejs/vue |
+| angular| https://github.com/angular/angular |
+
+
+[link text](https://github.com/jindada/markdown-for-react)
+
+[link with title](https://github.com/jindada/markdown-for-react "markdown-for-react")
+
+![Minion](https://octodex.github.com/images/minion.png)
+
+![Alt text][id]
+
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+
+## Contributing
+
+You can submit any ideas as [pull requests](https://github.com/jindada/markdown-for-react) or as a [GitLab issue](https://github.com/jindada/markdown-for-react/issues).
--- example/index.js
+++ example/index.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import { render } from 'react-dom';
 import MarkdownForReact from '../src/markdown-for-react';
-import './style.less'
+import './style.less';
 
 const value = `
-## \`markdown-for-react\`
-- __[markdown-it](https://nodeca.github.io/pica/demo/)__ - high quality and fast image resize in browser.
-- __[react-html-parser](https://github.com/nodeca/babelfish/)__ - developer friendly i18n with plurals support and easy syntax.
-- __[highlight.js](https://github.com/nodeca/babelfish/)__ - developer friendly i18n with plurals support and easy syntax.
+# h1 text
+## h2 text
+### h3 text
+#### h4 text
+##### h5 text
+###### h6 text
 
-## \`how to use\`
+
+"double quotes" and 'single quotes'
+
+**bold text**
+
+__bold text__
+
+*italic text*
+
+_italic text_
+
+~~Strikethrough~~
+
+> china
+>> heilongjiang
+>>> harbin
+
++ China
+  - heilongjiang
+    * harin
+    + daqing
+    - suihua
++ England
+
+1. react
+2. vue
+3. angular
+1. jquery
+1. css
+
+57. heilongjiang
+1. beijing
+
+    // hello world
+    line 1
+    line 2
+    line 3
+
 \`\`\` js
 import React from 'react';
 import { render } from 'react-dom';
@@ -18,103 +57,39 @@ import MarkdownForReact from 'markdown-for-react';
 class Example extends React.Component {
   render() {
     return (
-      <MarkdownForReact value={"# qweqwe"} />
+      <div>
+        <MarkdownForReact value={"# qweqwe"} />
+      </div>
     )
   }
 }
 render(<Example />, document.getElementById('init'));
 \`\`\`
 
-
-## \`For Example\`
-
-# h1 text
-## h2 text
-### h3 text
-#### h4 text
-##### h5 text
-###### h6 text
-
-test.. test... test..... test?..... test!....
-
-!!!!!! ???? ,,  -- ---
-
-"Smartypants, double quotes" and 'single quotes'
-
-**This is bold text**
-
-__This is bold text__
-
-*This is italic text*
-
-_This is italic text_
-
-~~Strikethrough~~
-
-> china
->> heilongjiang
->>> harbin
-
-+ Create a list by starting a line with \`+\`, \`-\`, or \`*\`
-+ Sub-lists are made by indenting 2 spaces:
-  - Marker character change forces new list start:
-    * Ac tristique libero volutpat at
-    + Facilisis in pretium nisl aliquet
-    - Nulla volutpat aliquam velit
-+ Very easy!
-
-Ordered
-
-1. react
-2. vue
-3. angular
-1. You can use sequential numbers...
-1. ...or keep all the numbers as \`1.\`
-
-Start numbering with offset:
-
-57. foo
-1. bar
-
-Inline \`code\`
-
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
-
-## Tables
-
-| Option | Description |
+| lib    |     url     |
 | ------ | ----------- |
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-
-Right aligned columns
-
-| Option | Description |
-| ------:| -----------:|
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
+| react  | https://github.com/facebook/react |
+| vuejs  | https://github.com/vuejs/vue |
+| angular| https://github.com/angular/angular |
 
 
-[link text](http://dev.nodeca.com)
+| lib     |     url      |
+| ------: | -----------: |
+| react  | https://github.com/facebook/react |
+| vuejs  | https://github.com/vuejs/vue |
+| angular| https://github.com/angular/angular |
 
-[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+[link text](https://github.com/jindada/markdown-for-react)
+
+[link with title](https://github.com/jindada/markdown-for-react "markdown-for-react")
 
 ![Minion](https://octodex.github.com/images/minion.png)
 
-Like links, Images also have a footnote style syntax
-
 ![Alt text][id]
-
-With a reference later in the document defining the URL location:
 
 [id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
 `;
-
 
 class Example extends React.Component {
 
